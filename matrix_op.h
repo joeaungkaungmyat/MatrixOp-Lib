0542ed5c0773cab104cb36cfd43ca07c65a3db86
joeaungkaungmyat <joeaungkaungmyat@gmail.com>
2025-12-19 14:21:27 +0800

    Update matrix_op.h

--- matrix_op.h
+++ matrix_op.h
@@ -1,12 +1,26 @@
 #ifndef MATRIX_OP_H
 #define MATRIX_OP_H
 
-#define SIZE 3 
+#define SIZE 3
+
 
 typedef float Matrix[SIZE][SIZE];
 
-void transpose(Matrix A, Matrix result);
-float determinant(Matrix A);
+
+void add_matrices(Matrix A, Matrix B, Matrix res);
+void subtract_matrices(Matrix A, Matrix B, Matrix res);
+
+
+void transpose(Matrix A, Matrix res);
+void multiply_matrices(Matrix A, Matrix B, Matrix res);
+void element_wise_multiply(Matrix A, Matrix B, Matrix res);
+
+
+float calculate_determinant(Matrix A);
+void get_adjoint(Matrix A, Matrix adj);
+int inverse_matrix(Matrix A, Matrix inv);
+
+
+void print_matrix(Matrix M, const char* name);
 
 #endif
-
